17b395142cfd8f31f2cd1f08d168aeba1ccc3d15
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-05-20 15:23:35 +0530

    Updates

--- assets/P-BKaxHhiB.js
+++
@@ -1,1 +0,0 @@
-import{o as s}from"./chunk-D4RADZKF-BlkkXO5c.js";function n({children:t,className:r="",...o}){return s.jsx("p",{...o,className:`text-ndark ${r}`,children:t})}export{n as P};
---
+++ assets/P-CHHTNPDk.js
@@ -0,0 +1,2 @@
+import{o}from"./chunk-D4RADZKF-BlkkXO5c.js";function l({children:t,className:x="",ref:e,...r}){return o.jsx("p",{ref:e,...r,className:`p-2 text-md
+        lg:text-lg xl:text-xl text-ndark ${x}`,children:t})}export{l as P};
